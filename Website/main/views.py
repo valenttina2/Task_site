69d0401417f3ba041d4e39ab266ab42afec0a5dd
Andrei <valuschika@gmail.com>
2022-08-25 23:58:03 +0300

    html-page

--- Website/main/views.py
+++ Website/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return render(request, 'about_us.html')
+    return render(request, 'main/about_us.html')
