04c59de93df7b58e7f67a5806d23816016bcc339
Andrei <valuschika@gmail.com>
2022-08-25 23:13:00 +0300

    page about_us

--- Website/main/views.py
+++ Website/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     return HttpResponse("<h4>Hello</h4>")
+
+def about(request):
+    return render(request, 'about_us.html')
