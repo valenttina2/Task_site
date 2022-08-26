31f18aac7746bd1a0ec0f442df2312fa08166415
Andrei <valuschika@gmail.com>
2022-08-26 17:08:22 +0300

    urls, model

--- Website/main/models.py
+++ Website/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField('Название', max_length=100)
+    task = models.TextField('Описание')
+    date = models.DateField('Дата окончания', max_length=50)
