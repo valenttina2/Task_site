3f3c64f2b4dd7399d0d10ad7977455b8fdab0e71
Andrei <valuschika@gmail.com>
2022-08-26 22:59:46 +0300

    models

--- Website/main/models.py
+++ Website/main/models.py
@@ -5,3 +5,10 @@ class Task(models.Model):
     title = models.CharField('Название', max_length=100)
     task = models.TextField('Описание')
     date = models.DateField('Дата окончания', max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
--- Website/main/views.py
+++ Website/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html',{'title':"Все задачи", 'tasks':tasks,})
 
 def about(request):
     return render(request, 'main/about_us.html')
