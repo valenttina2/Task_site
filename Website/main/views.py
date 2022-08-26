2ae657ad82d1a89ac7aa5a6e28c34d6ec7388f65
Andrei <valuschika@gmail.com>
2022-08-26 23:37:17 +0300

    create

--- Website/main/views.py
+++ Website/main/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from .models import Task
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('date')
     return render(request, 'main/index.html',{'title':"Все задачи", 'tasks':tasks,})
 
 def about(request):
     return render(request, 'main/about_us.html')
+
+def create(request):
+    return render(request, 'main/create.html')
