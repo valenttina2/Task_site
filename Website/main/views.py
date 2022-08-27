2a4e7b3f0e6157f10fe57472f76d6eee558448ea
Andrei <valuschika@gmail.com>
2022-08-27 16:09:02 +0300

    forms1

--- Website/main/views.py
+++ Website/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
     tasks = Task.objects.order_by('date')
@@ -10,4 +11,6 @@ def about(request):
     return render(request, 'main/about_us.html')
 
 def create(request):
-    return render(request, 'main/create.html')
+    form = TaskForm
+    context = {'form':form}
+    return render(request, 'main/create.html', context)
