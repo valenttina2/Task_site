9dee31454d1754540fa868d8663af3535ae42c15
Andrei <valuschika@gmail.com>
2022-08-28 22:55:25 +0300

    forms2

--- Website/main/forms.py
+++ Website/main/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 from .models import Task
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields=["title", "task", "date"]
+        widgets = {'title': TextInput(attrs={'class':'form-control',
+                                             'placeholder':'Введите название'}),
+            'task': TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Введите описание'}),
+            'date': TextInput(attrs={'class': 'form-control',
+                                     'placeholder': 'Введите дату окончания'}),
+
+        }
