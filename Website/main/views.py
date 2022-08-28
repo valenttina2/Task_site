380d8c88aff72cb28442425728cd279c1c81460b
Andrei <valuschika@gmail.com>
2022-08-28 23:43:46 +0300

    forms3

--- Website/main/views.py
+++ Website/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -11,6 +11,15 @@ def about(request):
     return render(request, 'main/about_us.html')
 
 def create(request):
-    form = TaskForm
-    context = {'form':form}
+    error = ''
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма заполнена некорректно"
+    form = TaskForm()
+    context = {'form':form,
+               'error':error}
     return render(request, 'main/create.html', context)
